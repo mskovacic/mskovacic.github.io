7c36a84c78850627ba5940d2c062f6d59cd95470
Martin Sven Kovačić <mskovacic10@gmail.com>
2026-01-22 21:14:31 +0100

    added subscription key

--- src/api.js
+++ src/api.js
@@ -21,31 +21,13 @@ async function sendChatMessage(messages) {
   }));
   const res = await fetch(BASE_URL + `/responses`, {
     method: 'POST',
-    headers: { 'Content-Type': 'text/plain' },
+    headers: { 'Content-Type': 'text/plain', 'api-key': '7e54d6f30b864cf598d34d39e931932b' },
     body: JSON.stringify(inputMessages)
   });
   if (!res.ok) {
     return Promise.reject({ status: res.status, data: await res.json() });
   }
   return res.body;
-  // const res = await fetch(BASE_URL + `/threads/${chatId}/messages?api-version=2025-03-01-preview`, {
-  //   method: 'POST',
-  //   headers: { 'Content-Type': 'application/json' },
-  //   body: JSON.stringify({ role: "user", content: message })
-  // });
-  // if (!res.ok) {
-  //   return Promise.reject({ status: res.status, data: await res.json() });
-  // }
-  // return res.body;
-
-  // asst_vQZmK8RNsa1BYWY8pP6vPAfU
-  // thread_Dy7kcMrlOGnpmivRnxCx166V
-  // run_juZPtbuVgSDgOXIyAEbM6AmV run_B2Bii7OQLP6foK4Vx4HusZM2
-
-  //previous_response_id=response.id, resp_05e8cb198ffaa207006953a095870c8195a25b1da3a6104f52
-
-  // /responses?api-version=2025-03-01-preview
-  // {"model":"gpt-5-nano","input":[{"type": "message", "role": "user", "content": "Define and explain the concept of catastrophic forgetting?"}],"stream":false,"previous_response_id":"resp_05e8cb198ffaa207006953a095870c8195a25b1da3a6104f52"}
 }
 
 async function getChatMessages(chatId) {
@@ -60,74 +42,30 @@ export default {
   createChat, sendChatMessage, getChatMessages
 };
 
-
 /*
-{
-    "id": "resp_05e8cb198ffaa207006953a0f2b06081958789e1129982e9e2",
-    "object": "response",
-    "created_at": 1767088370,
-    "status": "completed",
-    "background": false,
-    "content_filters": null,
-    "error": null,
-    "incomplete_details": null,
-    "instructions": null,
-    "max_output_tokens": null,
-    "max_tool_calls": null,
-    "model": "gpt-5-nano",
-    "output": [{
-            "id": "rs_05e8cb198ffaa207006953a0f318b0819592cc3662cd81d63f",
-            "type": "reasoning",
-            "summary": []
-        },
-        {
-            "id": "msg_05e8cb198ffaa207006953a0f798848195bf95103302c21147",
-            "type": "message",
-            "status": "completed",
-            "content": [{
-                "type": "output_text",
-                "annotations": [],
-                "logprobs": [],
-                "text": "The last user utterance before your current query was a request: \"Generate a 100 word text.\" It wasn\u2019t a question. If you meant a different last question, please share it and I\u2019ll answer."
-            }],
-            "role": "assistant"
-        }
-    ],
-    "parallel_tool_calls": true,
-    "previous_response_id": "resp_05e8cb198ffaa207006953a095870c8195a25b1da3a6104f52",
-    "prompt_cache_key": null,
-    "prompt_cache_retention": null,
-    "reasoning": {
-        "effort": "medium",
-        "summary": null
-    },
-    "safety_identifier": null,
-    "service_tier": "default",
-    "store": true,
-    "temperature": 1.0,
-    "text": {
-        "format": {
-            "type": "text"
-        },
-        "verbosity": "medium"
-    },
-    "tool_choice": "auto",
-    "tools": [],
-    "top_logprobs": 0,
-    "top_p": 1.0,
-    "truncation": "disabled",
-    "usage": {
-        "input_tokens": 152,
-        "input_tokens_details": {
-            "cached_tokens": 0
-        },
-        "output_tokens": 689,
-        "output_tokens_details": {
-            "reasoning_tokens": 640
-        },
-        "total_tokens": 841
-    },
-    "user": null,
-    "metadata": {}
-}
+SYSTEM MESSAGE
+Martin Sven Kovačić is a .NET developer with professional experience from 2016. 
+Here obtained his master's degree in computing in 2017. from Faculty of electrical engineering and computing in University of Zagreb, Croatia. 
+In 2015 he worked for 6 months as a student intern Data Analyst in ReversingLabs d.o.o. Zagreb; there he used Python to generate reports about internal metrics, he learnt a lot about malware, detection and cyber-security. 
+Beginning 2016 he worked in Farmeron d.o.o. for 3 months through an faculty-backed internship programme. 
+There he implemented a Web App, using .NET, ASP.NET and SQL Server technologies, for project management with Project, Sprint, Task and membership functionalities.
+In October 2016, he started working as a .NET intern in 'Eccos inženjering d.o.o.', Buzin, Croatia.
+There he worked on systems in the field of technical security.
+He learnt about entrance security, work reports, time-attendance and video-surveilance systems. 
+There he worked on projects for IKEA Croatia, Designer Outlet Croatia, Zagreb airport, HRT (Croatian broadband sender), A1 (ex Vip) telecommunications and different croatian ministries. 
+Upon graduating, Martin was promoted to full time .NET developer in Eccos. His projects became more complex and longer.
+In 2018 his biggest project was a real-time Web App for Slovenian Telecom's control center to manage escalations in monitored warehouses and datacenters, he worked in a team of 3 fullstack developers and used ASP.NET Core, SQL Server, event bus. In 2019 he lead team of (4 full-stak developers and interns) in a project for rent-a-car management on croatian airports; he decided on architecture questions, met with clients to discuss features, organized work into tickets and upon project completion held training for users.
+Even today that app is used on all major Croatian airports. 
+In 2021, Martin started work in Valcon Croatia d.o.o., an agency with Dutch owners and projects from Netherlands and Northern Europe.
+In 2022 he join a project, with 3 frontend developers, 3 backend developers and 2 QA-s, where he soon took over lead. 
+The goal of the project was a big-data analytics and management for smart buildings app.
+The App collected data from hundreds of building and hundreds of sensors on each buildings with the MQTT protocol and allowed users to instantly generate reports about building and device metrics or view the metrics in real-time. 
+Technologies used there were ASP.NET Core, Angular, PostgreSQL with sharding extensions, messaging service.  
+If you want to contact Martin, his email address in mskovacic10@gmail.com. 
+From other skills, Martin is proficient in Git, Scrum, Agile, Kanban, English and German language and has a drivers licence. 
+Martin is interested in work as and advanced senior or lead .NET developer. Martin's strengths are a calm head, being dependable, deep technical knowledge; his weakneses are being too dedicated to a project (private life suffers), too much thinking out-of-the-box (causes friction between team members), too work-a-holic in principle (might underestimate projects and timelines).
+Outside of work, Martin is a proud husband and father, from sports he practised sharpshooting for 12 years and loves Formula 1, in his free time he enjoys reading books, working on side-projects and building LEGOs with his son.
+You are his manager, a world class one.
+You have to answer questions truthfully and only about him, if you are asked a different topic switch the conversation to an interesting fact about Martin.
+Always respond in Markdown format."
 */
